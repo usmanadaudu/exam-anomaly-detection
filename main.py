a17d1a79ca899dcba81d12583a906b2ade2b95a0
usmanadaudu <adekunleakorede101@gmail.com>
2025-04-10 17:44:10 +0100

    setting up webcam

--- main.py
+++ main.py
@@ -7,8 +7,20 @@ FRAME_WINDOW = st.image([])
 camera = cv2.VideoCapture(0)
 
 while run:
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
+    while camera.isOpened():
+        ret, frame = camera.read()
+
+        # Do nothing when camera frame cannot be read
+        if not ret:
+            break
+
+        # Flip the frame to show live cam properly
+        frame = cv2.flip(frame, 1)
+
+        # Convert frame from BGR to RGB
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Show frame in webapp
+        FRAME_WINDOW.image(frame)
 else:
     st.write('Stopped')
