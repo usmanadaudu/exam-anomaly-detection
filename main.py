e1e12907425b10aab281f674038c5b140b6c4b22
usmanadaudu <adekunleakorede101@gmail.com>
2025-04-15 12:42:39 +0100

    debigging webcam

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-import cv2
-import streamlit as st
+# import cv2
+# import streamlit as st
 
-st.title("Webcam Live Feed")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-# camera = st.camera_input("Cheking camera input", disabled=not run)
+# st.title("Webcam Live Feed")
+# run = st.checkbox('Run')
+# FRAME_WINDOW = st.image([])
+# # camera = st.camera_input("Cheking camera input", disabled=not run)
 
-while run:
-    for i in range(5):
-        camera = cv2.VideoCapture(i)
-        if camera.isOpened():
-            print(f"Input {i} is a valid camera value for VIDEO_SOURCE")
-            ret, frame = camera.read()
+# while run:
+#     for i in range(1):
+#         camera = cv2.VideoCapture(i)
+#         if camera.isOpened():
+#             print(f"Input {i} is a valid camera value for VIDEO_SOURCE")
+#             ret, frame = camera.read()
 
-            # Do nothing when camera frame cannot be read
-            if not ret:
-                break
+#             # Do nothing when camera frame cannot be read
+#             if not ret:
+#                 break
 
-            # Flip the frame to show live cam properly
-            frame = cv2.flip(frame, 1)
+#             # Flip the frame to show live cam properly
+#             frame = cv2.flip(frame, 1)
 
-            # Convert frame from BGR to RGB
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#             # Convert frame from BGR to RGB
+#             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            # Show frame in webapp
-            FRAME_WINDOW.image(frame)
-        else:
-            st.write("Camera returned no frame...")
-else:
-    st.write('Stopped')
+#             # Show frame in webapp
+#             FRAME_WINDOW.image(frame)
+#         else:
+#             st.write("Camera returned no frame...")
+# else:
+#     st.write('Stopped')
+
+from streamlit_webrtc import webrtc_streamer
+
+webrtc_streamer(key="key")
