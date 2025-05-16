6cc2ab68876d7f30453ff24320d95a7e521701f4
Usman Daudu <67043752+usmanadaudu@users.noreply.github.com>
2025-05-16 20:06:05 +0100

    Comment out streamlit_webrtc and av

--- helpers.py
+++ helpers.py
@@ -1,12 +1,12 @@
 def check_pkg_versions():
-    import streamlit_webrtc
     import cv2
     import numpy as np
     import mediapipe as mp
-    import av
+    # import streamlit_webrtc 
+    # import av
 
-    print("streamlit_webrtc:", streamlit_webrtc.__version__)    
     print("cv2:",cv2.__version__)    
     print("numpy:", np.__version__)    
-    print("mediapipe:", mp.__version__)    
-    print("av:", av.__version__)
+    print("mediapipe:", mp.__version__)
+    # print("streamlit_webrtc:", streamlit_webrtc.__version__)     
+    # print("av:", av.__version__)
